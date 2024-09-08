8774c58ad66f7efcec7a3ff2db3a59b05aeb8efb
juanjk24 <juanjkuellar24@gmail.com>
2024-09-07 20:34:00 -0500

    First commit words count

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import './css/style.css'
+
+import { $ } from './utils/selector.js'
+
+const textarea = $('#input-text')
+const wordsCount = $('#words-count')
+const lettersCount = $('#letters-count')
+const spacesCount = $('#spaces-count')
+
+textarea.addEventListener('input', () => {
+    const text = textarea.value
+
+    // Split the text into words
+    const words = text.split(' ')
+    const word = words.filter(word => word !== '').length
+
+    // Split the text into letters
+    const letters = text.split('')
+    const letter = letters.filter(letter => letter !== ' ').length
+
+    // Count the spaces
+    const space = text.split(' ').length - 1
+
+    wordsCount.textContent = word
+    lettersCount.textContent = letter
+    spacesCount.textContent = space
+})
+
