38202005ddf9dbcfc50e60c5a054cad02cc491c1
juanjk24 <juanjkuellar24@gmail.com>
2024-10-08 00:49:59 -0500

    Add more styles for dialog

--- src/modal.js
+++ src/modal.js
@@ -1,23 +1,25 @@
-import { textarea, modal, deleteButton, cancelButton, confirmDeleteButton } from './utils/domElements.js'
-import { deleteText } from './deleteText.js'
+import {
+  textarea,
+  modal,
+  deleteButton,
+  cancelButton,
+  confirmDeleteButton,
+} from "./utils/domElements.js";
+import { deleteText } from "./deleteText.js";
 
+deleteButton.addEventListener("click", () => {
+  if (!textarea.value) {
+    return;
+  }
 
-deleteButton.addEventListener('click', () => {
-    if (!textarea.value) {
-        return
-    }
+  modal.showModal();
 
-    //modal.style.display = 'flex'
-    modal.showModal()
+  cancelButton.addEventListener("click", () => {
+    modal.close();
+  });
 
-    cancelButton.addEventListener('click', () => {
-        //modal.style.display = 'none'
-        modal.close()
-    })
-
-    confirmDeleteButton.addEventListener('click', () => {
-        deleteText()
-        //modal.style.display = 'none'
-        modal.close()
-    })
-})
+  confirmDeleteButton.addEventListener("click", () => {
+    deleteText();
+    modal.close();
+  });
+});
